5ef4436fe85a9014f29d69b8d726c966ebcaaeac
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2022-06-20 22:20:52 +0700

    everything

---
+++ app/src/main/java/com/dicoding/todoapp/data/Task.kt
@@ -0,0 +1,12 @@
+package com.dicoding.todoapp.data
+
+//TODO 1 : Define a local database table using the schema in app/schema/tasks.json
+
+@Entity()
+data class Task(
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val dueDateMillis: Long,
+    val isCompleted: Boolean = false
+)
